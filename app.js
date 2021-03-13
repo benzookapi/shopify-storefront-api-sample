ed0b4db95be2859ad229328cd73e4190fd765abe
Junichi Okamura <junichi.okamura@shopify.com>
2021-03-13 15:28:07 +0900

    #

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const serve = require('koa-static');
 const router = new Router();
 const app = module.exports = new Koa();
 
-const ADMIN_ENDPOINT  = `${process.env.SHOPIFY_ADMIN_ENDPOINT}`; // https://YOUR_API_KEY:YOUR_API_PASSWORD@YOUR_MYSHOPIFY_DOMAIN/admin/api/graphql.json
+const ADMIN_ENDPOINT  = `${process.env.SHOPIFY_ADMIN_ENDPOINT}`; // https://YOUR_MYSHOPIFY_DOMAIN/admin/api/graphql.json
+const ADMIN_TOKEN  = `${process.env.SHOPIFY_ADMIN_TOKEN}`; // COPIED_API_PASSWORD_FROM_YOUR_PRIVATE_APP
 const STOREFRONT_ENDPOINT  = `${process.env.SHOPIFY_STOREFRONT_ENDPOINT}`; // https://YOUR_MYSHOPIFY_DOMAIN/api/graphql.json
-const STOREFRONT_TOKEN  = `${process.env.SHOPIFY_STOREFRONT_TOKEN}`; // COPIED_ONE_FROM_YOUR_PRIVATE_APP
+const STOREFRONT_TOKEN  = `${process.env.SHOPIFY_STOREFRONT_TOKEN}`; // COPIED_TOKEN_FROM_YOUR_PRIVATE_APP
 
 
 app.use(bodyParser());
@@ -38,7 +39,7 @@ router.get('/one_pager',  async (ctx, next) => {
   console.log("+++++++++ /one_pager ++++++++++");
   let product_id = ctx.request.query.product_id;
 
-  let api_res = await(callGraphql(ctx, ADMIN_ENDPOINT, `{
+  let api_res = await(callGraphql(ctx, `{
     product(id: "gid://shopify/Product/${product_id}") {
       title
       handle
@@ -54,21 +55,27 @@ router.get('/one_pager',  async (ctx, next) => {
         }
       }    
     }
-  }`));
+  }`, false));
   console.log(`${JSON.stringify(api_res)}`);        
   await ctx.render('one_pager', {
   });
 });
 
 // https://shopify.dev/docs/storefront-api/getting-started
-const callGraphql = function(ctx, endpoint, req, vars = null, storefront_token = null) {
+const callGraphql = function(ctx, req, vars = null, storefront = true) {
+  let endpoint = STOREFRONT_ENDPOINT;
+  let token = STOREFRONT_TOKEN;
+  if (!storefront) {
+    endpoint = ADMIN_ENDPOINT;
+    token = ADMIN_TOKEN;
+  }  
   let api_req = {};
   // Set Gqphql string into query field of the JSON  as string
   api_req.query = req.replace(/\n/g, '');
   if (vars != null) {
     api_req.variables = vars;
   }
-  console.log(`callGraphql ${endpoint} ${storefront_token} ${JSON.stringify(api_req)}`);
+  console.log(`callGraphql ${endpoint} ${token} ${JSON.stringify(api_req)}`);
   return new Promise(function(resolve, reject) { 
     // Success callback
     let then_func = function(res){
@@ -82,7 +89,11 @@ const callGraphql = function(ctx, endpoint, req, vars = null, storefront_token =
     };
     let headers = {};
     headers['Content-Type'] = 'application/json'; // for JSON.stringify otherwise application/graphql
-    headers['X-Shopify-Storefront-Access-Token'] = storefront_token;
+    if (storefront) {
+      headers['X-Shopify-Storefront-Access-Token'] = token;
+    } else {
+      headers['X-Shopify-Access-Token'] = token;
+    }    
     ctx.post(endpoint, api_req, headers).then(then_func).catch(catch_func);   
   });
 };   

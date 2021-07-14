9b485404d64ae7bcd980cfebfada51fb673cc2d8
Junichi Okamura <junichi.okamura@shopify.com>
2021-07-15 03:00:29 +0900

    #

--- app.js
+++ app.js
@@ -224,15 +224,33 @@ router.post('/carrier_service',  async (ctx, next) => {
   //ctx.body = "OK";
   ctx.body = {
     "rates": [
-      {
-        "service_name": 'Service Name',
-        "description": 'Description',
-        "service_code": 'Service_Code',
-        "currency": 'JPY',
-        "total_price": '777700'
-      }
+        {
+            "service_name": "canadapost-overnight",
+            "service_code": "ON",
+            "total_price": "1295",
+            "description": "This is the fastest option by far",
+            "currency": "CAD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-2dayground",
+            "service_code": "2D",
+            "total_price": "2934",
+            "currency": "USD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        },
+        {
+            "service_name": "fedex-priorityovernight",
+            "service_code": "1D",
+            "total_price": "3587",
+            "currency": "USD",
+            "min_delivery_date": "2013-04-12 14:48:45 -0400",
+            "max_delivery_date": "2013-04-12 14:48:45 -0400"
+        }
     ]
-  }
+ };
 });
 
 // https://shopify.dev/docs/storefront-api/getting-started
